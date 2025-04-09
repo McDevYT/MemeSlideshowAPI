72341e8161b563a54f0b4cbaf0e3a1ec72850787
Gabriel Hollenstein <gabriel.hollenstein@vertigis.com>
2025-04-09 09:51:27 +0200

    Update

--- images/index.js
+++ images/index.js
@@ -5,7 +5,7 @@ const pool = require("./db");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // Setup Multer to store files in /images
 const storage = multer.diskStorage({
