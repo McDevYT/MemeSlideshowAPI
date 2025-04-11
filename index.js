07d121c1819723a2d19058d0470d4e9c774d30c3
Gabriel Hollenstein <gabriel.hollenstein@vertigis.com>
2025-04-11 14:11:50 +0200

    sadf

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 4000;
 
-const loopingQueue = [];
+let loopingQueue = [];
 const sendNextQueue = [];
 let fun = false;
 
